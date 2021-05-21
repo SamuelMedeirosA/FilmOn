8b3a1b3d2f2cbbea821f4fba2b57cee56049fdd1
Samuel Medeiros de Albuquerque <84237653+Samm014@users.noreply.github.com>
2021-05-21 05:10:21 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,68 @@
+
+
+
+/* slider */
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    center:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:2,
+            center:true
+        },
+        600:{
+            center: true,
+            items:3
+        },
+        1000:{
+            center: true,
+            items:5
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$(function(){
+    alert("Jquery carregado com sucesso!")
+})*/
